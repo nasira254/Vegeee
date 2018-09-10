12b60a5872bacf2b7ba63ff8dac2667635912629
Nasir Ali <nasirali@Nasirs-MacBook-Pro.local>
2018-09-10 22:09:39 +0100

    authorisation added

---
+++ Vegeee/RegisterViewController.swift
@@ -0,0 +1,59 @@
+//
+//  RegisterViewController.swift
+//  Vegeee
+//
+//  Created by Nasir Ali on 10/09/2018.
+//  Copyright © 2018 Nasir Ali. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class RegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            }else {
+                //success
+                print("Registration Success")
+                
+                
+            }
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
