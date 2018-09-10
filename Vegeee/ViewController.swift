f9bb29d7318387f576ccf7eb59f2241d979a56a1
Nasir Ali <nasirali@nasirs-macbook-pro.local>
2018-09-10 12:04:14 +0100

    Initial Commit

---
+++ Vegeee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vegeee
+//
+//  Created by Nasir Ali on 10/09/2018.
+//  Copyright © 2018 Nasir Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
