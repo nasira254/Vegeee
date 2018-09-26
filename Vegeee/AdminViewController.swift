7067fce1e0ff97b63635ef512fbf010bd7482f78
Nasir Ali <nasirali@nasirs-macbook-pro.local>
2018-09-26 14:59:13 +0100

    complete

--- Vegeee/AdminViewController.swift
+++ Vegeee/AdminViewController.swift
@@ -1,6 +1,6 @@
 //
 //  AdminViewController.swift
-//  
+//
 //
 //  Created by Nasir Ali on 25/09/2018.
 //
@@ -12,21 +12,20 @@ class AdminViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     var refItems : DatabaseReference!
     
+//
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemList.count
         
     }
-    
+    //to dsiplay the content in tablew view using the item model created
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
         let items : ItemModel
         items = itemList[indexPath.row]
-        
-        
+        // all the label in the selected row
         cell.labelName.text = items.name
         cell.labelPrice.text = items.price
-        
-        
+        //to call the values in that cell
         return cell
     }
     
@@ -47,7 +46,7 @@ class AdminViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         let updateAction = UIAlertAction(title: "update", style: .default) { (_) in
             
-        let id = item.id
+            let id = item.id
             
             let name = alertController.textFields?[0].text
             let price = alertController.textFields?[1].text
@@ -84,49 +83,48 @@ class AdminViewController: UIViewController, UITableViewDelegate, UITableViewDat
         messageLabel.text = "item updated"
         
     }
-    
+    //to delete item by item id selected
     func deleteItem(id: String){
         refItems.child(id).setValue(nil)
-        
-        
-        
     }
     @IBAction func addButtonPressed(_ sender: Any) {
+        //function called when button is pressed
         addItems()
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        //setting category in firebase db items
+        refItems = Database.database().reference().child("items")
         
-
-        // Do any additional setup after loading the view.
-          refItems = Database.database().reference().child("items")
-        
+       //observing if any items in the category and display
         refItems.observe(DataEventType.value, with: {(snapshot) in
+            
             if snapshot.childrenCount>0 {
                 self.itemList.removeAll()
                 
+                // setting object values
                 for items in snapshot.children.allObjects as! [DataSnapshot]{
                     let itemObject = items.value as? [String: AnyObject]
                     let itemName = itemObject? ["ItemName"]
                     let itemPrice = itemObject? ["ItemPrice"]
                     let itemId = itemObject? ["id"]
                     
+                    //setting data type
                     let item = ItemModel(id: itemId as! String?, name: itemName as! String?, price: itemPrice as! String?)
                     
+                    //making a liost
                     self.itemList.append(item)
                 }
+                //then loading that list available
                 self.tableItems.reloadData()
-                
-                
             }
         })
-        
-        
     }
-
+    
+    //function to add vegetable items
     func addItems(){
-        
+        //setting values
         let key = refItems.childByAutoId().key
         let items = ["id" : key,
                      "ItemName" : itemNameTextField.text! as String,
@@ -134,30 +132,14 @@ class AdminViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         refItems.child(key).setValue(items)
         
+        //label message to say item added
         messageLabel.text = "Item added";
     }
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
-    
     }
-
-        
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
--- Vegeee/ItemModel.swift
+++ Vegeee/ItemModel.swift
@@ -5,19 +5,15 @@
 //  Created by Nasir Ali on 26/09/2018.
 //  Copyright © 2018 Nasir Ali. All rights reserved.
 //
-
 class ItemModel{
     
     var id: String!
     var name: String!
     var price: String!
     
-    
     init(id:String?, name:String?, price: String?) {
         self.id = id;
         self.name = name;
         self.price = price;
-        
-        
     }
 }
